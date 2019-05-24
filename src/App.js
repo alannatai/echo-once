350233298c29ca3dea951e52ea146bfd7b67a992
Alanna Tai <alanna.tai@hotmail.com>
2019-05-24 18:01:17 -0400

    Connect api quote data to code

--- src/App.js
+++ src/App.js
@@ -5,39 +5,37 @@ import Button from './components/button/button';
 import Header from './components/header/header';
 import NavBar from './components/navbar/navbar';
 
-const quotes = [
-  {
-    quote: '"I\'m not the strongest. I\'m not the fastest. But I\'m really good at suffering."',
-    name: 'Amelia Boone'
-  },
-  {
-    quote: '"It\'s unfortunate that this has happened. No. It\'s fortunate that this has happened and I\'ve remained unharmed."',
-    name: 'Marcus Aurelius'
-  },
-  {
-    quote: '"...there\'s only one really good question, which is, \'What am I unwilling to feel?\'"',
-    name: 'Tara Brach'
-  },
-  {
-    quote: '"You must want to be a butterfly so badly, you are willing to give up being a caterpillar."',
-    name: 'Sekou Andrews'
-  },
-  {
-    quote: '"Learn the rules like a pro, so you can break them like an artist."',
-    name: 'Pablo Picasso'
-  }
-]
+//let quotes = [];
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      index: quotes.length - 1
+      quotes: [
+        {
+          quote: '"Learn the rules like a pro, so you can break them like an artist."',
+          name: 'Pablo Picasso'
+        }
+      ],
+      index: null
     };
     this.handleClick = this.handleClick.bind(this);
   }
   
   componentDidMount() {
+    fetch('http://localhost:3000/quotes')
+      .then(response => response.json())
+      .then(res => {
+        if (res && res.data) {
+          this.setState(
+            {
+              quotes: [...this.state.quotes, ...res.data], 
+              index: this.state.quotes.length - 1
+            }
+          );
+        };
+      });
+
     setTimeout(() => this.setState({
       isInitiated: true,
     }), 2000);
@@ -45,7 +43,7 @@ class App extends Component {
 
   handleClick() {
     this.setState({
-      index: this.state.index === 0 ? (quotes.length -1) : (this.state.index -1)
+      index: this.state.index === 0 ? (this.state.quotes.length -1) : (this.state.index -1)
     })
   }
 
@@ -54,7 +52,7 @@ class App extends Component {
       <div className="App">
         <NavBar />
         <Header />
-        {this.state.isInitiated && <Quotes quote={quotes[this.state.index].quote} name={`- ${quotes[this.state.index].name}`} />} 
+        {this.state.isInitiated && <Quotes quote={this.state.quotes[this.state.index].quote} name={`- ${this.state.quotes[this.state.index].name}`} />} 
         <Button onClick={this.handleClick} />
       </div>
     );
