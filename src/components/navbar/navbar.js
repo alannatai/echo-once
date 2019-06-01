3ab1a937fcbe8bcdf1004ce13f8cddfff7f3028c
Alanna Tai <alanna.tai@hotmail.com>
2019-06-01 18:45:36 -0400

    Refactor navbar links with map function

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,22 +2,37 @@ import React from 'react';
 import './navbar.css';
 import { NavLink } from 'react-router-dom';
 
-export default function NavBar(props) {
+const links = [
+    {
+        path: '/',
+        name: 'Home'
+    },
+    {
+        path: '/about',
+        name: 'About'
+    },
+    {
+        path: '/submit',
+        name: 'Submit'
+    },
+    {
+        path: '/login',
+        name: 'Log In'
+    },
+];
+
+let navLinks = 
+    links.map(link => {
+        return (<li>
+            <NavLink exact to={link.path} className="link" activeStyle={{color: "pink", fontWeight: "bold"}}>{link.name}</NavLink>
+        </li>)
+    });
+
+export default function NavBar(props) {    
     return (
         <nav>
             <ul>
-                <li>
-                    <NavLink exact to="/" className="link" activeStyle={{color: "pink", fontWeight: "bold"}}>Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/about" className="link" activeStyle={{color: "pink", fontWeight: "bold"}}>About</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/submit" className="link" activeStyle={{color: "pink", fontWeight: "bold"}}>Submit</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/login" className="link" activeStyle={{color: "pink", fontWeight: "bold"}}>Log In</NavLink>
-                </li>
+                {navLinks}
             </ul>
         </nav>
     )
